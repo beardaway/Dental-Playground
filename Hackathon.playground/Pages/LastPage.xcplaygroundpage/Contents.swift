80f99918028a3cade97cf1bc4965828d63ecf9ec
beardaway <konrad.sopala@gmail.com>
2018-03-24 19:28:42 +0100

    Last page refactored + finished

--- Hackathon.playground/Pages/LastPage.xcplaygroundpage/Contents.swift
+++ Hackathon.playground/Pages/LastPage.xcplaygroundpage/Contents.swift
@@ -6,8 +6,9 @@ import AVFoundation
 
 struct Constants {
     static let preferredTextSize: CGFloat = 63.0
-    static let preferredEmojiSize: CGFloat = 100.0
-    static let preferredEmoji: String = "😱"
+    static let preferredFont: String = "Avenir-Heavy"
+    static let headsHeight: Int = 120
+    static let headsWidth: Int = 120
 }
 
 let liveViewCanvas: UIView = UIView(frame: CGRect(x: 0, y: 0, width: 1024, height: 892))
@@ -16,18 +17,20 @@ liveViewCanvas.backgroundColor = UIColor.orange
 let finishLabel: UILabel = UILabel(frame: CGRect(x: 120, y: -370, width: 700, height: 70))
 finishLabel.text = "To be continued..."
 finishLabel.textColor = UIColor.white
-finishLabel.font = UIFont(name: "Avenir-Heavy", size: Constants.preferredTextSize)
+finishLabel.font = UIFont(name: Constants.preferredFont, size: Constants.preferredTextSize)
 liveViewCanvas.addSubview(finishLabel)
 
-let screamLabelOne: UILabel = UILabel(frame: CGRect(x: 320, y: 110, width: 100, height: 100))
-screamLabelOne.text = Constants.preferredEmoji
-screamLabelOne.font = UIFont(name: "Avenir-Heavy", size: Constants.preferredEmojiSize)
-liveViewCanvas.addSubview(screamLabelOne)
+let konradHead: UIImageView = UIImageView(image: UIImage(named: "KonradHead"))
+konradHead.frame = CGRect(x: 50, y: 190, width: Constants.headsWidth, height: Constants.headsHeight)
+liveViewCanvas.addSubview(konradHead)
 
-let screamLabelTwo: UILabel = UILabel(frame: CGRect(x: 320, y: 610, width: 100, height: 100))
-screamLabelTwo.text = Constants.preferredEmoji
-screamLabelTwo.font = UIFont(name: "Avenir-Heavy", size: Constants.preferredEmojiSize)
-liveViewCanvas.addSubview(screamLabelTwo)
+let yaoHead: UIImageView = UIImageView(image: UIImage(named: "YaoHead"))
+yaoHead.frame = CGRect(x: 580, y: 190, width: Constants.headsWidth, height: Constants.headsHeight)
+liveViewCanvas.addSubview(yaoHead)
+
+let blakeHead: UIImageView = UIImageView(image: UIImage(named: "BlakeHead"))
+blakeHead.frame = CGRect(x: 310, y: 500, width: Constants.headsWidth, height: Constants.headsHeight)
+liveViewCanvas.addSubview(blakeHead)
 
 func animateFinishLabel() -> Void {
     UIView.animate(withDuration: 2.0) {
@@ -37,15 +40,15 @@ func animateFinishLabel() -> Void {
 
 func rotateScreamingFaces() -> Void {
     UIView.animate(withDuration: 1.2, delay: 0.0, options: .curveLinear, animations: {
-        screamLabelOne.transform = screamLabelOne.transform.rotated(by: CGFloat(Double.pi))
-        screamLabelTwo.transform = screamLabelTwo.transform.rotated(by: CGFloat(Double.pi))
+        konradHead.transform = konradHead.transform.rotated(by: CGFloat(Double.pi))
+        yaoHead.transform = yaoHead.transform.rotated(by: CGFloat(Double.pi))
+        blakeHead.transform = blakeHead.transform.rotated(by: CGFloat(Double.pi))
     }) { (finished) in
         rotateScreamingFaces()
     }
 }
 
 func tellStory() -> Void {
-    
     let storyTeller: AVSpeechSynthesizer = AVSpeechSynthesizer()
     let storyText: AVSpeechUtterance = AVSpeechUtterance(string: "Here starts the story of great anticipation")
     
