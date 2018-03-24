a5311402fcbaab17d7880d66f2034465ef74750a
beardaway <konrad.sopala@gmail.com>
2018-03-24 18:51:04 +0100

    FirstDayPage finished - code to refactor.

--- Hackathon.playground/Pages/FirstDayPage.xcplaygroundpage/Contents.swift
+++ Hackathon.playground/Pages/FirstDayPage.xcplaygroundpage/Contents.swift
@@ -1,7 +1,8 @@
-//: [Previous](@previous)
+//: [Previous page 🤓](@previous)
 
 import UIKit
 import PlaygroundSupport
+import AVFoundation
 
 let liveViewCanvas: UIView = UIView(frame: CGRect(x: 0, y: 0, width: 1024, height: 892))
 liveViewCanvas.backgroundColor = UIColor.white
@@ -50,6 +51,48 @@ let treeFour: UIImageView = UIImageView(image: UIImage(named: "MediumTree"))
 treeFour.frame = CGRect(x: 12, y: 600, width: 130, height: 200)
 liveViewCanvas.addSubview(treeFour)
 
+let konradMini: UIImageView = UIImageView(image: UIImage(named: "KonradMini"))
+konradMini.frame = CGRect(x: 200, y: 700, width: 100, height: 180)
+liveViewCanvas.addSubview(konradMini)
+
+let blakeMini: UIImageView = UIImageView(image: UIImage(named: "BlakeMini"))
+blakeMini.frame = CGRect(x: 350, y: 670, width: 100, height: 180)
+liveViewCanvas.addSubview(blakeMini)
+
+let yaoMini: UIImageView = UIImageView(image: UIImage(named: "YaoMini"))
+yaoMini.frame = CGRect(x: 480, y: 700, width: 100, height: 180)
+liveViewCanvas.addSubview(yaoMini)
+
+let konradChat: UIImageView = UIImageView(image: UIImage(named: "RedChatCloud"))
+konradChat.frame = CGRect(x: 200, y: 265, width: 110, height: 80)
+liveViewCanvas.addSubview(konradChat)
+
+let konradChatText: UILabel = UILabel(frame: CGRect(x: 210, y: 262, width: 90, height: 60))
+konradChatText.textColor = UIColor.white
+konradChatText.font = UIFont(name: "Avenir-Heavy", size: 17)
+konradChatText.text = "Let's do it!"
+liveViewCanvas.addSubview(konradChatText)
+
+let blakeChat: UIImageView = UIImageView(image: UIImage(named: "OrangeChatCloud"))
+blakeChat.frame = CGRect(x: 320, y: 280, width: 110, height: 80)
+liveViewCanvas.addSubview(blakeChat)
+
+let blakeChatText: UILabel = UILabel(frame: CGRect(x: 342, y: 277, width: 70, height: 60))
+blakeChatText.textColor = UIColor.white
+blakeChatText.font = UIFont(name: "Avenir-Heavy", size: 20)
+blakeChatText.text = "Finally!"
+liveViewCanvas.addSubview(blakeChatText)
+
+let yaoChat: UIImageView = UIImageView(image: UIImage(named: "RedChatCloud"))
+yaoChat.frame = CGRect(x: 440, y: 265, width: 110, height: 80)
+liveViewCanvas.addSubview(yaoChat)
+
+let yaoChatText: UILabel = UILabel(frame: CGRect(x: 450, y: 255, width: 110, height: 80))
+yaoChatText.textColor = UIColor.white
+yaoChatText.font = UIFont(name: "Avenir-Heavy", size: 17)
+yaoChatText.text = "Can't wait!"
+liveViewCanvas.addSubview(yaoChatText)
+
 let teslaImageView: UIImageView = UIImageView(image: UIImage(named: "Car"))
 teslaImageView.frame = CGRect(x: 900, y: 763, width: 400, height: 130)
 liveViewCanvas.addSubview(teslaImageView)
@@ -81,12 +124,12 @@ liveViewCanvas.addSubview(sunRayFive)
 let sunRays: [UIImageView] = [sunRayOne, sunRayTwo, sunRayThree, sunRayFour, sunRayFive]
 
 func sunShine() -> Void {
-    UIView.animate(withDuration: 2.0, animations: {
+    UIView.animate(withDuration: 1.5, animations: {
         for ray in sunRays {
             ray.alpha = 0
         }
     }) { (completed) in
-        UIView.animate(withDuration: 2.0, delay: 1.0, options: UIViewAnimationOptions.curveEaseOut, animations: {
+        UIView.animate(withDuration: 1.5, delay: 1.0, options: UIViewAnimationOptions.curveEaseOut, animations: {
             for ray in sunRays {
                 ray.alpha = 1
             }
@@ -96,15 +139,95 @@ func sunShine() -> Void {
     }
 }
 
-func driveGuysToHackathon() -> Void {
+func driveHackersToHackathon() -> Void {
     UIView.animate(withDuration: 7.0) {
         teslaImageView.transform = CGAffineTransform(translationX: -720, y: 0)
     }
+    
+    DispatchQueue.main.asyncAfter(deadline: .now() + 7) {
+        getHackersOutOfTheCar()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3, execute: {
+            driveCarAway()
+        })
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3, execute: {
+           takeHackersToHackathonVenue()
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3, execute: {
+                UIView.animate(withDuration: 2.0, animations: {
+                    getHackersTalking()
+                })
+            })
+        })
+    }
 }
 
+func driveCarAway() -> Void {
+    UIView.animate(withDuration: 5.0) {
+        teslaImageView.transform = CGAffineTransform(translationX: 500, y: 0)
+    }
+}
+
+func keepHackersInTheCar() -> Void {
+    konradMini.alpha = 0
+    blakeMini.alpha = 0
+    yaoMini.alpha = 0
+}
+
+func getHackersOutOfTheCar() -> Void {
+    UIView.animate(withDuration: 4.0) {
+        konradMini.alpha = 1
+        blakeMini.alpha = 1
+        yaoMini.alpha = 1
+    }
+}
+
+func takeHackersToHackathonVenue() -> Void {
+    UIView.animate(withDuration: 3.0) {
+        konradMini.transform = CGAffineTransform(translationX: 0, y: -350)
+        blakeMini.transform = CGAffineTransform(translationX: 0, y: -300)
+        yaoMini.transform = CGAffineTransform(translationX: 0, y: -350)
+    }
+}
+
+func keepHackersQuite() -> Void {
+    konradChat.alpha = 0
+    konradChatText.alpha = 0
+    blakeChat.alpha = 0
+    blakeChatText.alpha = 0
+    yaoChat.alpha = 0
+    yaoChatText.alpha = 0
+}
+
+func getHackersTalking() -> Void {
+    konradChat.alpha = 1
+    konradChatText.alpha = 1
+    blakeChat.alpha = 1
+    blakeChatText.alpha = 1
+    yaoChat.alpha = 1
+    yaoChatText.alpha = 1
+}
+
+func tellStory() -> Void {
+    let storyTeller: AVSpeechSynthesizer = AVSpeechSynthesizer()
+    let storyText: AVSpeechUtterance = AVSpeechUtterance(string: "Once upon a time there was a lot of students who were doing their best to get scholarships to Dub-Dub in San Jose. Once upon a time there was a lot of students who were doing their best to get scholarships to Dub-Dub in San Jose. Once upon a time there was a lot of students who were doing their best to get scholarships to Dub-Dub in San Jose")
+    
+    storyText.rate = AVSpeechUtteranceDefaultSpeechRate;
+    storyText.voice = AVSpeechSynthesisVoice(language: "en-US")
+    
+    guard storyTeller.isSpeaking else {
+        storyTeller.speak(storyText)
+        return
+    }
+}
+
+tellStory()
 sunShine()
-driveGuysToHackathon()
+keepHackersInTheCar()
+keepHackersQuite()
+driveHackersToHackathon()
 
 PlaygroundPage.current.liveView = liveViewCanvas
 
-//: [Next](@next)
+//: [Next page 🤓](@next)
